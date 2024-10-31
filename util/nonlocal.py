13b53d9b76f9f353081ef355a26f0c80806331d1
yyf20000904 <76048962+yyf20000904@users.noreply.github.com>
2024-10-31 16:10:28 +0800

    Add files via upload

--- util/nonlocal.py
+++ util/nonlocal.py
@@ -1,7 +1,7 @@
 import torch
 from torch import nn
 from torch.nn import functional as F
-
+import pdb
 
 class _NonLocalBlockND(nn.Module):
     def __init__(self, in_channels, inter_channels=None, dimension=3, sub_sample=True, bn_layer=True):
@@ -20,52 +20,52 @@ class _NonLocalBlockND(nn.Module):
             if self.inter_channels == 0:
                 self.inter_channels = 1
 
-        if dimension == 3:
-            conv_nd = nn.Conv3d
-            max_pool_layer = nn.MaxPool3d(kernel_size=(1, 2, 2))
-            bn = nn.BatchNorm3d
-        elif dimension == 2:
-            conv_nd = nn.Conv2d
-            max_pool_layer = nn.MaxPool2d(kernel_size=(2, 2))
-            bn = nn.BatchNorm2d
-        else:
-            conv_nd = nn.Conv1d
-            max_pool_layer = nn.MaxPool1d(kernel_size=(2))
-            bn = nn.BatchNorm1d
+        # if dimension == 3:
+        #     conv_nd = nn.Conv3d
+        #     max_pool_layer = nn.MaxPool3d(kernel_size=(1, 2, 2))
+        #     bn = nn.BatchNorm3d
+        # elif dimension == 2:
+        # conv_nd = nn.Conv2d
+        max_pool_layer = nn.MaxPool2d(kernel_size=(2, 2))
+        bn = nn.BatchNorm2d
+        # else:
+        #     conv_nd = nn.Conv1d
+        #     max_pool_layer = nn.MaxPool1d(kernel_size=(2))
+        #     bn = nn.BatchNorm1d
 
-        self.g = conv_nd(in_channels=self.in_channels, out_channels=self.inter_channels,
+        self.g = nn.Conv2d(in_channels=self.in_channels, out_channels=self.inter_channels,
                          kernel_size=1, stride=1, padding=0)
 
         if bn_layer:
             self.W = nn.Sequential(
-                conv_nd(in_channels=self.inter_channels, out_channels=self.in_channels,
+                nn.Conv2d(in_channels=self.inter_channels, out_channels=self.in_channels,
                         kernel_size=1, stride=1, padding=0),
                 bn(self.in_channels)
             )
             nn.init.constant_(self.W[1].weight, 0)
             nn.init.constant_(self.W[1].bias, 0)
         else:
-            self.W = conv_nd(in_channels=self.inter_channels, out_channels=self.in_channels,
+            self.W = nn.Conv2d(in_channels=self.inter_channels, out_channels=self.in_channels,
                              kernel_size=1, stride=1, padding=0)
             nn.init.constant_(self.W.weight, 0)
             nn.init.constant_(self.W.bias, 0)
 
-        self.theta = conv_nd(in_channels=self.in_channels, out_channels=self.inter_channels,
+        self.theta = nn.Conv2d(in_channels=self.in_channels, out_channels=self.inter_channels,
                              kernel_size=1, stride=1, padding=0)
 
-        self.phi = conv_nd(in_channels=self.in_channels, out_channels=self.inter_channels,
+        self.phi = nn.Conv2d(in_channels=self.in_channels, out_channels=self.inter_channels,
                            kernel_size=1, stride=1, padding=0)
 
         if sub_sample:
-            self.g = nn.Sequential(self.g, max_pool_layer)
-            self.phi = nn.Sequential(self.phi, max_pool_layer)
+            self.g = nn.Sequential(self.g, nn.MaxPool2d(kernel_size=(2, 2)))
+            self.phi = nn.Sequential(self.phi, nn.MaxPool2d(kernel_size=(2, 2)))
 
     def forward(self, x):
         '''
         :param x: (b, c, t, h, w)
         :return:
         '''
-
+        # pdb.set_trace()
         batch_size = x.size(0)
 
         g_x = self.g(x).view(batch_size, self.inter_channels, -1)
